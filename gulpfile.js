6caa825b9569901712527c5ade7cbcb1e7c58fe1
AZG <abdelaziz.gh@gmail.com>
2016-04-04 11:09:46 +0000

    Minify, Uglify, Build

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
 var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+
 var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+
 
 var paths = {
   sass: ['app/sass/*.scss','!src/sass/_require.scss'],
-  jade: 'app/jade/views/**/*.jade'
+  jade: 'app/jade/views/**/*.jade',
+  css : 'output/assets/css/*.css', //only first level
+  js : 'output/assets/js/*.js', //only first level
 };
 
 //SASS TASK
@@ -28,10 +34,18 @@ gulp.task('jade', function(){
 
 //Minify CSS
 gulp.task('minify-css', function() {
-  return gulp.src('output/assets/css/*.css')
+  return gulp.src(paths.css)
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('output/assets/css/'));
+});
+
+//Uglify JS
+gulp.task('uglify-js', function() {
+  return gulp.src(paths.js)
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('output/assets/js/'));
 });
 
 //Copying
@@ -40,9 +54,16 @@ gulp.task('src-build', function() {
     .pipe(gulp.dest('output/'));
 });
 
+
+//Build projects
+gulp.task('build', function(callback){
+    runSequence('src-build',['jade','sass'],['minify-css','uglify-js'], callback)
+});
+
+
 //Watching tasks
 gulp.task('watch', function(){
   gulp.watch('app/sass/**/*.scss', ['sass']); //watch all Sass 
   gulp.watch('app/jade/**/*.jade', ['jade']); //watch all Jade 
-  gulp.watch('app/src/**/*', ['src-build','minify-css']); //watch all modifs in src 
+  gulp.watch('app/src/**/*', ['src-build','minify-css','uglify-js']); //watch all modifs in src 
 })
